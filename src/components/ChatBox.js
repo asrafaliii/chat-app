afc279896815204316bf04dcf127817b0a255534
Asraf Ali <moha.asrafali@gmail.com>
2022-08-26 13:17:38 +0600

    create firestore db

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -1,15 +1,21 @@
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase";
+import Chat from "./Chat";
 import Navbar from "./Navbar";
 
 const style = {
   appContainer: `max-w-[728px] mx-auto text-center`,
   sectionContainer: `flex flex-col h-[95vh] bg-gray-700 mt-5 shadow-xl border relative`,
 };
+
 const ChatBox = () => {
+  const [user] = useAuthState(auth);
   return (
     <div className={style.appContainer}>
       <section className={style.sectionContainer}>
         <Navbar />
+        {user ? <Chat /> : null}
       </section>
     </div>
   );
